1c70b9f766ab47004e807961dab6c259e74cfd9f
martin seal <martin.seal@spicatech.co.uk>
2021-02-15 08:39:57 +0000

    CLEAN view model test

--- app/src/androidTest/java/com/sealstudios/spacex/ui/viewmodels/CompanyViewModelTest.kt
+++ app/src/androidTest/java/com/sealstudios/spacex/ui/viewmodels/CompanyViewModelTest.kt
@@ -8,9 +8,7 @@ import com.sealstudios.spacex.repositories.SpaceXRepository
 import com.sealstudios.spacex.testUtils.getValueBlocking
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.internalSubstitute
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
@@ -37,7 +35,7 @@ class CompanyViewModelTest {
     fun getCompany() {
         runBlocking {
             coEvery { spaceXRepository.getCompanyResponse() } returns Resource.loading(null)
-            val companyViewModel =  CompanyViewModel(spaceXRepository = spaceXRepository)
+            val companyViewModel = CompanyViewModel(spaceXRepository = spaceXRepository)
             val companyResource = companyViewModel.company.getValueBlocking()
             assertThat(companyResource?.status, equalTo(Status.LOADING))
         }
